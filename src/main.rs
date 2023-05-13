c75de6f2c6cbb01a9cfd9827f04af31ea2b94dcc
Rafael Linnankoski <rafaellinnankoski@gmail.com>
2023-05-13 17:24:20 +0900

    Separate handler

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use axum::{
-    routing::get,
-    Router,
-};
+use axum::{http::StatusCode, routing::get, Router};
+
+async fn handler() -> (StatusCode, String) {
+    println!("Hello from handler");
+    (StatusCode::OK, String::from("Hello reload"))
+}
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", get(handler));
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
